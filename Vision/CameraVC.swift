5fad81f29fadf883b78114d7922c4e45c1e54c2a
Jeremy Clerico <blops2hd@gmail.com>
2017-08-12 09:58:34 +0100

    First Commit

---
+++ Vision/CameraVC.swift
@@ -0,0 +1,27 @@
+//
+//  CameraVC.swift
+//  Vision
+//
+//  Created by Jeremy Clerico on 12/08/2017.
+//  Copyright © 2017 Jeremy Clerico. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
